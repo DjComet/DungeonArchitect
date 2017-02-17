d75dbd2f6f7a0404fb11b00a5d5d0be349f591b3
DjComet <alberto.abad.sanz@gmail.com>
2017-02-17 14:05:52 +0100

    Organizacion del proyecto. Nuevo proyecto.

---
+++ Dungeon Architect/Assets/Scripts/ApplicationQuit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApplicationQuit : MonoBehaviour {
+
+    public void Exit()
+    {
+		Debug.Log ("Exiting application");
+        Application.Quit();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+			Exit();
+        }
+	}
+}
