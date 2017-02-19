f478d50e42f3cb9e2a29a9bfc1784e49f8a40dea
DjComet <alberto.abad.sanz@gmail.com>
2017-02-19 19:26:06 +0100

    si

---
+++ Dungeon Architect/Assets/Scripts/Accelerated_movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accelerated_movement : MonoBehaviour {
+
+    public float acceleration = 2;
+    public float maxSpeed = 50;
+    private float Speed = 0;
+    private float time = Time.deltaTime;
+	
+	// Update is called once per frame
+	void Update () {
+
+        if ((Input.GetKey(KeyCode.A)) && (Speed < maxSpeed))
+        {
+
+            Speed -= acceleration * time;
+        }
+        else if ((Input.GetKey(KeyCode.D)) && (Speed > -maxSpeed))
+        {
+            Speed += acceleration * time;
+
+        }
+        
+
+
+
+    }
+}
