ce3d22a28fa72808a905b47a5e051188f9092804
DjComet <alberto.abad.sanz@gmail.com>
2017-02-19 16:58:59 +0100

    U wot

---
+++ Dungeon Architect/Assets/Scripts/blocMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class blocMovement : MonoBehaviour {
+	public int objVel = 10;
+	// Use this for initialization
+
+	void Start () {
+	}
+	// Update is called once per frame
+	void Update () {
+		if (transform.localPosition.y>0) {
+			transform.Translate (Vector3.down * objVel * Time.deltaTime);
+		}
+	}
+}
